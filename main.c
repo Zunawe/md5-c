594d4d262d842831907e0ba4a5b7626d4b734222
Zunawe <gyroscope15@gmail.com>
2017-02-08 15:50:49 -0700

    Use fread instead of getdelim If any arguments are provided, they are interpreted as strings

--- main.c
+++ main.c
@@ -4,44 +4,30 @@
 
 #include "md5.h"
 
-/*int main(int argc, char *argv[]){
-	if(argc < 2){
-		printf("Usage: ./md5 [STRING] ...\n");
-		return 0;
+int main(int argc, char *argv[]){
+	if(argc > 1){
+		for(int i = 1; i < argc; ++i){
+			MD5Context ctx;
+			uint8_t *input = (uint8_t *)argv[i];
+
+			md5Init(&ctx);
+			md5Update(&ctx, input, strlen(argv[i]));
+			md5Finalize(&ctx);
+			print_hash(ctx.digest);
+		}
 	}
+	else{
+		char *input = malloc(1024);
+		size_t input_size = 0;
 
-	for(int i = 1; i < argc; ++i){
 		MD5Context ctx;
-		uint8_t *input = (uint8_t *)argv[i];
-
 		md5Init(&ctx);
-		md5Update(&ctx, input, strlen(argv[i]));
+
+		while((input_size = fread(input, 1, 1024, stdin)) > 0){
+			md5Update(&ctx, (uint8_t *)input, input_size);
+		}
+
 		md5Finalize(&ctx);
 		print_hash(ctx.digest);
 	}
-}*/
-
-int main(void){
-	char *input = NULL;
-	size_t input_size = 0;
-	ssize_t read = 0;
-
-	if((read = getdelim(&input, &input_size, -1, stdin)) == -1){
-		free(input);
-		perror("Error reading from stdin");
-		exit(1);
-	}
-
-	if(ferror(stdin)){
-		free(input);
-		perror("Error reading from stdin");
-		exit(1);
-	}
-
-	MD5Context ctx;
-	md5Init(&ctx);
-
-	md5Update(&ctx, (uint8_t *)input, read);
-	md5Finalize(&ctx);
-	print_hash(ctx.digest);
 }
