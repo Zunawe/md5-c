61012f1be2b11f701d5867877b04e8293917776d
Zunawe <gyroscope15@gmail.com>
2017-02-08 15:22:13 -0700

    Input is stdin

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 #include "md5.h"
 
-int main(int argc, char *argv[]){
+/*int main(int argc, char *argv[]){
 	if(argc < 2){
 		printf("Usage: ./md5 [STRING] ...\n");
 		return 0;
@@ -19,29 +19,9 @@ int main(int argc, char *argv[]){
 		md5Finalize(&ctx);
 		print_hash(ctx.digest);
 	}
-}
-
-/*int main(int argc, char *argv[]){
-	uint8_t *input;
-	if(argc < 2){
-		input = calloc(1, 1);
-	}
-	else{
-		input = (uint8_t *)argv[1];
-	}
-
-	uint8_t *result = md5(input);
-
-	print_hash(result);
-
-	// Free if we had to allocate space
-	if(argc < 2){
-		free(input);
-	}
-	free(result);
 }*/
 
-/*int main(void){
+int main(void){
 	char *input = NULL;
 	size_t input_size = 0;
 	ssize_t read = 0;
@@ -55,13 +35,13 @@ int main(int argc, char *argv[]){
 	if(ferror(stdin)){
 		free(input);
 		perror("Error reading from stdin");
-		exit(2);
+		exit(1);
 	}
-	//input[read - 1] = 0;
 
-	uint8_t *result = md5((uint8_t *)input);
+	MD5Context ctx;
+	md5Init(&ctx);
 
-	print_hash(result);
-	free(input);
-	free(result);
-}*/
+	md5Update(&ctx, (uint8_t *)input, read);
+	md5Finalize(&ctx);
+	print_hash(ctx.digest);
+}
