8026bda713e5bf8afe927bdce33a55bb2cb19b2d
MilchRatchet <milchratchet@googlemail.com>
2023-12-30 21:53:09 +0100

    Added const qualifier to applicable function parameters.

--- md5.h
+++ md5.h
@@ -14,11 +14,11 @@ typedef struct{
 }MD5Context;
 
 void md5Init(MD5Context *ctx);
-void md5Update(MD5Context *ctx, uint8_t *input, size_t input_len);
+void md5Update(MD5Context *ctx, const uint8_t *input, size_t input_len);
 void md5Finalize(MD5Context *ctx);
-void md5Step(uint32_t *buffer, uint32_t *input);
+void md5Step(uint32_t *buffer, const uint32_t *input);
 
-void md5String(char *input, uint8_t *result);
+void md5String(const char *input, uint8_t *result);
 void md5File(FILE *file, uint8_t *result);
 
 #endif
